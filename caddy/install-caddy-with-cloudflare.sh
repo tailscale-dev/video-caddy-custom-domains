4e2c5a4278fdb73f7ad19c61adb6c554db1c26ff
Alex Kretzschmar <2773080+ironicbadger@users.noreply.github.com>
2024-10-24 12:48:12 -0400

    Update install-caddy-with-cloudflare.sh

--- caddy/install-caddy-with-cloudflare.sh
+++ caddy/install-caddy-with-cloudflare.sh
@@ -17,3 +17,13 @@ else
     curl "https://caddyserver.com/api/download?os=linux&arch=amd64&p=github.com%2Fcaddy-dns%2Fcloudflare" -o $caddy_path
     chmod +x $caddy_path
 fi
+
+cp caddy.service /etc/systemd/system/caddy.service
+systemctl daemon-reload
+mkdir -p /etc/caddy
+useradd caddy
+mkdir /home/caddy
+chown -R caddy:users /home/caddy
+
+echo "You'll want to modify you Caddyfile next..."
+echo "Then copy it to `/etc/caddy/Caddyfile`"
